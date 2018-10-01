cddb4da194ac4412a046c4988048dd718cee2352
Brain <brainnovocaine@msn.com>
2018-09-30 22:41:47 -0600

    Second Commit

--- friendsApp/data/friends.js
+++ friendsApp/data/friends.js
@@ -1,64 +1,63 @@
 // Your friend data goes here
-
 var friendsArray = [
   {
     name: "Garry",
     photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVPl0H9ms-ECztJa-rqFjl790okQdV4mC1SV3dm9HpLN6lR5JQBQ",
+      "https://images.pexels.com/photos/1362908/pexels-photo-1362908.jpeg?auto=compress&cs=tinysrgb&h=350",
     scores: [3, 1, 5, 3, 5, 3, 3, 2, 3, 2]
   },
   {
     name: "Barry",
     photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs8DJfVfLKKWoJO5dTZ19CzOq2JiQrKdMKW6hfRbUbUtzEy_ge",
+      "https://images.pexels.com/photos/256621/pexels-photo-256621.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
     name: "Jerry",
     photo:
-      " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpe_NF9Jb9lIBLJbKikTGj5bavrJDEK1es1s0KQ-jfvC-2z9T-",
+      "https://images.pexels.com/photos/209620/pexels-photo-209620.jpeg?auto=compress&cs=tinysrgb&h=350",
     scores: [2, 1, 3, 1, 5, 1, 2, 3, 1, 1]
   },
   {
     name: "Terry",
     photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6duBIpfDRbn6Qb_0VZpZHySHpKj9uZEnwoISnyt_jdpuN8fh1",
+      "https://images.pexels.com/photos/52507/alcohol-hangover-event-death-52507.jpeg?auto=compress&cs=tinysrgb&h=350",
     scores: [5, 4, 4, 4, 5, 5, 5, 5, 4, 4]
   },
   {
     name: "Perry",
     photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpnjBTiBjkKplK0bmjSmqWz9h5HDx53urvgj_SiAxv_v-TKHaR",
+      "https://photos.gograph.com/thumbs/CSP/CSP891/guy-with-a-mug-of-beer-eps-vector_k8914634.jpg",
     scores: [5, 3, 4, 4, 5, 4, 3, 4, 4, 2]
   },
   {
     name: "Marry",
     photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBauIyE8FiDx12i8q3vqi3dU-13spMjMtIwKV7emCucPkic96XrQ",
+      "https://photos.gograph.com/thumbs/CSP/CSP741/alcoholic-vector-stock_k7416308.jpg",
     scores: [2, 2, 3, 2, 2, 1, 3, 2, 4, 2]
   },
   {
     name: "Kerry",
     photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHzIGPI_JwEOQApPxK1Xt4dqjfzf2qtj3OdNTkz-eT6Ch71WPi4g",
+      "https://photos.gograph.com/thumbs/CSP/CSP755/valkyrie-drunk-vector-art_k20643057.jpg",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
     name: "Gus",
     photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVPl0H9ms-ECztJa-rqFjl790okQdV4mC1SV3dm9HpLN6lR5JQBQ",
+      "https://photos.gograph.com/thumbs/CSP/CSP995/drunk-man-vector-illustration_k16919710.jpg",
     scores: [5, 1, 1, 1, 5, 5, 5, 5, 4, 5]
   },
   {
     name: "Aubrey",
     photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPJ2VBXSMwlndsbO7LSfVCVrR6ZOAyZ-xPR2AfTh6SQS1eF0qd",
+      "http://www.clker.com/cliparts/b/d/3/f/15161646691533231543free-clipart-of-drunks.med.png",
     scores: [3, 3, 2, 4, 3, 3, 4, 4, 4, 4]
   },
   {
     name: "Sherry",
     photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVPl0H9ms-ECztJa-rqFjl790okQdV4mC1SV3dm9HpLN6lR5JQBQ",
+      "https://images.pexels.com/photos/1230397/pexels-photo-1230397.jpeg?auto=compress&cs=tinysrgb&h=350",
     scores: [5, 3, 3, 3, 2, 5, 5, 5, 1, 4]
   }
 ];
@@ -73,5 +72,114 @@ console.log(friendsArray[7].scores.reduce((a, b) => a + b, 0));
 console.log(friendsArray[8].scores.reduce((a, b) => a + b, 0));
 console.log(friendsArray[9].scores.reduce((a, b) => a + b, 0));
 
+console.log(friendsArray[9].scores);
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendsArray;
+
+//     // not sure we need a .each(function (i, element){ if (this.selectedIndex = 0) }) here, we do want to loop through and find what was selected though
+
+// $(".btn btn-lg btn-primary float-right").on("click", function () {
+// Example from StackOverflow:
+
+//             <select id="mySelect" multiple="multiple">
+//   <option value="1">First</option>
+//   <option value="2">Second</option>
+//   <option value="3">Third</option>
+//   <option value="4">Fourth</option>
+// </select>
+
+// <script type="text/javascript">
+// $(document).ready(function() {
+//   if (!$("#mySelect option:selected").length) {
+//     $("#mySelect option[value='3']").attr('selected', 'selected');
+//   }
+// });
+// </script>
+
+//         var userInput1 = $(".dropdown-header col-sm-1").val();
+//         var userInput2 = [];
+//         userInput2.push(userInput1);
+//         console.log(userInput);
+//         // not sure if this will be used
+//         // function multiplyArray(num) {
+//         //     return num * userInput2;
+//         // }
+//         var friends = [];
+//         console.log(friends);
+//         $.get('/data/friends', function (data) {
+
+//             friends.push(data);
+//         })
+
+//         $.post("/api/friends", function (req, res) {
+
+//             var matchName = '';
+//             var matchPic = '';
+//             var totalDiff = 10;
+
+//             for (var i = 0; i < friends.length; i++) {
+//                 console.log('friend = ' + JSON.stringify(friends[i]));
+
+//                 var diffy = 0;
+//                 for (var j = 0; j < userInput2.length; j++) {
+//                     diffy += Math.abs(friends[i].scores[j] - userInput2[j]);
+//                 }
+//                 console.log(diffy);
+//                 if (diffy < totalDiff) {
+
+//                     totalDiff = diffy;
+
+//                     matchName = friends[i].name,
+//                         matchPic = friends[i].photo
+
+//                 }
+//             }
+
+//             console.log("object being sent: ", { matchName: matchName, matchPic: matchPic });
+//             console.log("friends: ", friends);
+//             console.log("userInput: ", userInput2);
+
+//         }).done(function (data) {
+//             console.log(data);
+//             friends.push(userInput2);
+//             res.send({ status: 200, matchName: matchName, matchPic: matchPic });;
+
+//         })
+
+//         // console.log(compare);
+//         // for (var i = 0; i < userInput2; i++) {
+//         //     for (var y = 0; y < compare; y++) {
+//         //         // var w =Math.min(userInput[i], compare2[y]);
+//         //         // var z = userInput2[i] - compare2.length[y];
+//         //         var x = math.min.apply(null, userInput2[i].map(Math.abs), compare[y].map(Math.abs));
+//         //         empty.push(map(x));
+//         //         console.log(empty);
+//         //     }
+
+//         // } console.log(x);
+//         // console.log(empty);
+//         // if (empty.reduce((a, b) => a + b, 0) === userInput2)
+//         //     $.post("/friendsPage", userInput2)
+
+//     })
+// }
+// function reset() { $("select").each(function () { this.selectedIndex = 0 }) }
+
+//  Using a RegEx Pattern to remove spaces from searchedCharacter You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+// userInput = userInput.replace(/\s+/g, "").toLowerCase(); QUESTION: What does $.get do? What are the parameters it is expecting?
+// Must have a .get to draw from the phony friends data to manipulate it here! BodyParser makes sure all of the data is uniform
+// from req and res unless you are trying to manipulate a new array, then it will have to be converted into an object. Handlbars
+// only manipulates objects.All of your code, pretty much needs to be in the .get and .post on-click submit .post uses req.body
+// or req.params. You can insert user submitted info into req.body and 'get' it on the server page data the client sends to
+// the server is .post remember post uses req.body and re.params console.log(data); var newArray = []; if (data) {for (var i
+// = 0; i < userInput.length; i++){for (var y=0 ; y < data.scores; y++){ var z=M ath.min(userInput[i], data.scores[y]; } $(
+//     "#stats").show(); $( "#name").text(data.name); $( "#role").text(data.role); $( "#age").text(data.age); $( "#force-points").text(data.forcePoints);
+//     some examples of functions that get and function renderFriendsPage(req, res) { fs.readFile(__dirname + "/index.html", function(err,
+//     data) { if (err) { res.writeHead(500, { "Content-Type": "text/html" }); res.end( "
+// We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+//  an html file. res.writeHead(200, { "Content-Type ": "text/html
+//     " }); res.end(data); } }); }
+// POST EXAMPLE - function renderThankYouPage(req, res) {Saving the request posted data as a variable. var requestData = " "; var myHTML = " ";  When the server receives data, it will add it to requestData. req.on( "data ", function(data) { requestData
+//             +=d ata; console.log( "You just posted some data to the server:\n ", requestData); myHTML=" When the request
+//     has ended... req.on( " end ", function() { res.writeHead(200, { "Content-Type ": "text/html
+//             " }); res.end(myHTML);
--- friendsApp/routes/htmlRoutes.js
+++ friendsApp/routes/htmlRoutes.js
@@ -1,23 +1,161 @@
 var path = require("path");
-
+var friends = require("../data/friends.js");
 module.exports = function(app) {
+  // If no matching route is found default to home
+  // app.get( function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/index.html"));
+  // });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
   // 'REQ' BELOW EQUATES TOO: ROUTE THAT THE USER SELECTS/ENTERS
-  app.get("/friendsPage", function(req, res) {
-    res.sendFile(path.join(__dirname, "friendsPage"));
+  // app.get("/friendsPage", function(req, res) {
+  //   res.sendFile(
+  //     "C:/Users/Brain/Desktop/HomeWork11/friendsApp/public/friendsPage.html"
+  //   );
+  // });
+  app.get("/api/friends", function(req, res) {
+    var myHTML =
+      "<html><footer><button onclick='goBack()'> Go Back </button></footer></html>";
+    myHTML += "<script> function goBack(){window.history.back()}</script>";
+
+    res.json(friends);
+
+    // function doIt() {
+    //   if (myHTML) {
+    //     function goBack() {
+    //       window.history.back();
+    //     }
+    //   } else {
+    //     throw console.error();
+    //   }
+    // }
+    // res.render("/api/friends", myHTML);
   });
-  //   app.get("/data/friends", function(req, res) {
-  //     console.log(res);
-  //     res.json(res);
-  //   });
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index"));
+  //   res.json(friends);
+
+  app.post("/api/friends", function(req, res) {
+    if (res) {
+      res.push(req.body);
+      res.json(true);
+    } else {
+      res.json(false);
+    }
+    // var reqInfo = req.body;
+
+    // var result = reqInfo.map(function(x) {
+    //   return parseInt(x, 10);
+    // });
+
+    // console.log(result);
+    // res.json(friends);
+    console.log("friendsArray", friends);
+    console.log("req.body", req.body);
+    // var filterInt = reqInfo.each(function(value) {
+    //   if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
+    //     return newArray.push(Number(value));
+    //   return NaN;
+    // });
+    // try {
+    //   newArray.push(JSON.parse(reqInfo)); // this is how you parse a string into JSON
+    // } catch (ex) {
+    //   console.error(ex);
+    // }
+    // for (i = 0; i < reqInfo; i++) {
+    //   return Number(reqInfo[i]);
+    // }
+
+    // var filterInt = function(value) {
+    //   if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value)) return Number(value);
+    //   return NaN;
+    // };
+    // console.log(filterInt(req.body));
+    // var reqInfo = parseInt(req.body);
+    // var newArray = [];
+    // newArray.push(reqInfo);
+    // var i = reqInfo.length;
+    // while (i--) !/\S/.test(reqInfo[i]) && reqInfo.splice(i, 1);
+    // console.log(reqInfo);
+    // function removeWhiteSpaceFromArray(reqInfo) {
+    //   return reqInfo.filter(item => item != " ");
+    //   console.log(reqInfo);
+    // }
+    // removeWhiteSpaceFromArray();
+    // var i = reqInfo.length;
+    // while (i--) !/\S/.test(reqInfo[i]) && reqInfo.splice(i, 1);
+    // reqInfo = reqInfo
+    //   .filter(function(item) {
+    //     return item !== "\n";
+    //   })
+    //   .map(function(item) {
+    //     return item.replace(/\n/g, "");
+    //   });
+    // reqInfo.filter(function(str) {
+    //     return /\S/.test(str);
+    //     });
+
+    // newArray
+    //   .split(" ")
+    //   .join("")
+    //   .split("");
+
+    //         $.post("/api/friends", function (req, res) {
+
+    //             var matchName = '';
+    //             var matchPic = '';
+    //             var totalDiff = 10;
+
+    //             for (var i = 0; i < friends.length; i++) {
+    //                 console.log('friend = ' + JSON.stringify(friends[i]));
+
+    //                 var diffy = 0;
+    //                 for (var j = 0; j < userInput2.length; j++) {
+    //                     diffy += Math.abs(friends[i].scores[j] - userInput2[j]);
+    //                 }
+    //                 console.log(diffy);
+    //                 if (diffy < totalDiff) {
+
+    //                     totalDiff = diffy;
+
+    //                     matchName = friends[i].name,
+    //                         matchPic = friends[i].photo
+
+    //                 }
+    //             }
+
+    //             console.log("object being sent: ", { matchName: matchName, matchPic: matchPic });
+    //             console.log("friends: ", friends);
+    //             console.log("userInput: ", userInput2);
+
+    //         }).done(function (data) {
+    //             console.log(data);
+    //             friends.push(userInput2);
+    //             res.send({ status: 200, matchName: matchName, matchPic: matchPic });;
+
+    //         })
+
+    // req.params
+    // This property is an object containing properties mapped to the named route “parameters”. For example, if you have the route /user/:name, then the “name” property is available as req.params.name. This object defaults to {}.
+
+    // req.body
+    // Contains key-value pairs of data submitted in the request body. By default, it is undefined, and is populated when you use body-parsing middleware such as body-parser and multer.
+
+    // var newFriend = req.body.matchName;
+    // var newFriendImage = req.body.matchPic;
+    // res.json({ newFriend: newFriend, newFriendImage: newFriendImage });
   });
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index"));
+  app.post("/clear", function() {
+    // Empty out the arrays of data
+    arr = [];
   });
 };
+
+//   app.get("/data/friends", function(req, res) {
+//     console.log(res);
+//     res.json(res);
+//   });
